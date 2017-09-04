6c5202885c51296e762144af3307ebb28e27d2a2
Davanon <davanon2014@gmail.com>
2017-09-04 11:43:52 +0530

    First

---
+++ src/main/scala/com/iqmsoft/payara/scala/Main.scala
@@ -0,0 +1,22 @@
+
+package com.iqmsoft.payara.scala
+
+import javax.json.JsonObject
+import javax.json.Json
+import javax.ws.rs._
+import javax.ws.rs.core._
+
+@ApplicationPath("api")
+class MyApp extends Application
+
+@Path("test")
+class TestResource {
+
+  @GET
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def test(@QueryParam("name") name: String): JsonObject = {
+    return Json.createObjectBuilder()
+      .add("msg", "Test Payara Micro Scala " + name)
+      .build()
+  }
+}
